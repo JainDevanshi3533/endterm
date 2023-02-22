bf9757f983f8d87c82b5fab5461462a97e72717d
Devanshi <devanshi3533.be20@chitkara.edu.in>
2023-02-22 20:57:50 +0530

    styling and layout done

--- video-chat/server.js
+++ video-chat/server.js
@@ -1,38 +1,38 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const { v4 : uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 const io = require('socket.io')(server);
 // const {ExpressPeerServer} = require('peer');
 // const peerServer = ExpressPeerServer(server, {
 //     debug:true
 // });
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 // app.use(express.json());
 app.use(express.static('public'));
 // app.use('/peerjs', peerServer);
-app.get('/', (req,res)=>{
-    res.redirect(`/${uuidv4()}`)
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidv4()}`);
 });
 
-app.get('/:room', (req,res)=>{
-    res.render('room', {roomId : req.params.room})
-})
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room });
+});
 
-io.on('connection',socket =>{
-    socket.on('join-room', (roomId, userId) =>{
-        // console.log("joined room");
-        socket.join(roomId);
-        socket.to(roomId).emit('user-connected', userId);
-    })
-})
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId, userId) => {
+    // console.log("joined room");
+    socket.join(roomId);
+    socket.to(roomId).emit('user-connected', userId);
+  });
+});
 server.listen(2222);
 
 /*  to run...
-
+        // i have already installed peer as global dependency for this... using.....  npm i -g peer
 peerjs --port 3001
-        and then
+        // and then
 nodemon server.js
 
 */
